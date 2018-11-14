5df03721cee7093c89df0d2f1dde3e5d1991344f
gerryRcom <gerryr@gerryr.com>
2018-11-14 22:48:31 +0000

    Network address discovery code working

--- pi-map-process.sh
+++ pi-map-process.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-if [ -e /var/www/html/commandqueue/initialScan.cmd ]
+#Source of function: https://stackoverflow.com/questions/50413579/bash-convert-netmask-in-cidr-notation
+IPprefix_by_netmask() {
+        #function returns prefix for given netmask in arg1
+         ipcalc -nb 1.1.1.1 $1 | sed -n '/Netmask/s/^.*=[ ]/\//p'
+}
+ipAdd=$(/sbin/ifconfig eth0 | awk '/inet /{print $2}')
+ipMask=$(/sbin/ifconfig eth0 | awk '/inet /{print $4}')
+ipCIDR=$(IPprefix_by_netmask $ipMask)
+if [ -e /var/www/html/commandqueue/quickScan.cmd ]
         then
+                echo $ipAdd
+                echo $ipMask
+                echo $ipCIDR
                 pkill python
                 python /home/pi/pi-map/startLED.py
-                mv /var/www/html/commandqueue/initialScan.cmd /var/www/html/commandqueue/running/
+                mv /var/www/html/commandqueue/quickScan.cmd /var/www/html/commandqueue/running/
                 rm /var/www/html/commandqueue/running/default.cmd
-                sudo nmap -O -oX /var/www/html/initialScan.xml 192.168.50.50
+                sudo nmap -O -oX /var/www/html/initialScan.xml $ipAdd$ipCIDR
                 python /home/pi/pi-map/stopLED.py
-                rm /var/www/html/commandqueue/running/initialScan.cmd
+                rm /var/www/html/commandqueue/running/quickScan.cmd
 #elif [ $2 == 'yes' ]
         #then
            #        echo You may go to the party but be back before midnight.
@@ -21,6 +32,5 @@ elif [ -e /var/www/html/commandqueue/running/*.cmd ]
 else
         sudo touch /var/www/html/commandqueue/running/default.cmd
         /usr/bin/python /home/pi/pi-map/ipLED.py $(bash /home/pi/pi-map/getIP.sh)
-        #sudo touch /var/www/html/commandqueue/running/default.cmd
 
 fi
