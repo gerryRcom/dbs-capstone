f7b197015744712ae0e1ac43e5085011aa20114b
gerryRcom <gerryr@gerryr.com>
2018-11-16 22:36:25 +0000

    Worked on main GUI page

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
 <html><head><title>Test php</title></head><body>
-</br></br>
-        <a href="command.php?cmd=quickScan">Run ping scan of network (quick)</a></br>
+</br>
+<p>
+<img src="images/logo_pi-map.png" alt="Pi-Map Logo" titel="Pi-Map Logo" style="float:right;width:280px;height:240px;">
+<h2>Welcome to Pi-Map, hover over the controls below for more information and please note the Pi LED screen has two modes which will inform you of the current status:</h2>
+<ul><li>Ready mode where the Pi-Map's current IP address will scroll accross the screen, no commands are running when this is displayed.</li>
+<li>Scan mode, the word 'SCAN' will be displayed on the screen, when in this mode the device is running a scan or processing something.  Once it ends you might have to refresh the page to see the results.</li></ul>
+</br>
+</p>
+        <a href="command.php?cmd=quickScan"><img src="images/button_quick-network-scan.png" alt="Perform a quick discover scan of the current subnet (fast scan)" title="Perform a quick discovery scan of the current subnet (fast scan)"></a></br>
         <a href="command.php?cmd=cmd1">Command 1</a></br>
+
+<h3>Scan Results</h3>
+
 
 <?php
         if (file_exists('initialScan.xml')) {
         $xml = simplexml_load_file('initialScan.xml');
         foreach ($xml->host as $discoveredHost){
 
-                print_r("<a href=\"command.php?cmd=".(string)$discoveredHost->address[0]['addr']."\">Deep Scan Of: ".(string)$discoveredHost->address[0]['addr']."</a>");
-                print_r('</br>');
+                print_r("Dicovered device IP: ".(string)$discoveredHost->address[0]['addr']);
+                print_r("</br>");
+                print_r("Discovered device MAC: ".(string)$discoveredHost->address[1]['addr']." MAC Vendor: ".(string)$discoveredHost->address[1]['vendor']);
+                print_r("</br>");
+                print_r("<a href=\"command.php?cmd=".(string)$discoveredHost->address[0]['addr']."\"><img src=\"images/button_full-device-scan.png\" alt=\"Perform a full scan of this device\" title=\"Perform a full scan of this device\"></a>");
+                if (file_exists((string)$discoveredHost->address[0]['addr'].".xml")){
+                        print_r("<a href=\"device.php?cmd=".(string)$discoveredHost->address[0]['addr']."\"><img src=\"images/button_device-scan-results.png\" alt=\"View full device scan results\" title=\"View full device scan results\"></a>");
+                }
+                print_r('</br></br>');
         }
 } else {
     exit('No previous scan results to display');
