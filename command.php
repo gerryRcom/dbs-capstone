d89981ede30c4f472b5e8b912709f7734ea822f0
gerryRcom <gerryr@gerryr.com>
2018-11-21 21:58:58 +0000

    Added time to execute indicator on command page

--- command.php
+++ command.php
@@ -1,9 +1,9 @@
 <?php
-
+$secondCountdown=60 - date("s");
 if($_REQUEST['cmd'] == 'quickScan')
 	{
 		shell_exec('touch /var/www/html/commandqueue/quickScan.cmd');
-		echo "Initial scan running, please wait and return to GUI once scan is complete</br>";
+		echo "Initial scan will begin in approx ".$secondCountdown." seconds, please wait and return to GUI once scan is complete</br>";
 		echo "The scan might take some time depending on how many hosts being scanned</br>";
 		echo "You'll know the scan is complete as the -SCAN- will stop displaying on the LED display</br>";
 		echo "<a href=\"index.php\">Return to main GUI</a>";
@@ -11,16 +11,15 @@ if($_REQUEST['cmd'] == 'quickScan')
 else if($_REQUEST['cmd'] == 'archiveResults')
 	{
 		shell_exec('touch /var/www/html/commandqueue/archiveResults.cmd');
-		echo "Archiving previous results, please wait";
+		echo "Archiving previous results, please wait, task will run in approx ".$secondCountdown." seconds</br>";
 		echo "<a href=\"index.php\">Return to main GUI</a>";
-	}
-else if($_REQUEST['cmd'] == 'cmd2')
-	{
-		echo "command 2 running, please wait";
 	}
 else
 	{
 		shell_exec('touch /var/www/html/commandqueue/'.$_GET['cmd'].'.cmd');
+		echo "Device scan will begin in approx ".$secondCountdown. " seconds, pleasee wait and return to the main GUI once scan is complete</br>";
+		echo "You will know the scan is complete as the -SCAN- message will stop displaying on the LED display</br>";
+		echo "<a href=\"index.php\">Return to main GUI</a>";
 	}
 
 ?>
