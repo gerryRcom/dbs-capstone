cbef32a824bfadeb2e2fa566bb7553c5c533e858
gerryRcom <gerryr@gerryr.com>
2019-01-07 22:42:54 +0000

    Resolved scanning issue with multiple devices selected

--- pi-map/pi-map-process.sh
+++ pi-map/pi-map-process.sh
@@ -45,23 +45,27 @@ elif [ -e /var/www/html/commandqueue/archiveResults.cmd ]
 #Used compgen as opposed to -e as -e does not work with wildcard/ multiple files
 elif compgen -G "/var/www/html/commandqueue/*.cmd" > /dev/null
 	then
-		for device in /var/www/html/commandqueue/*.cmd
+		mv /var/www/html/commandqueue/*.cmd /var/www/html/commandqueue/running/
+		for device in /var/www/html/commandqueue/running/*.cmd
 		do
 			deviceIP=$(basename $device .cmd)
+			echo $deviceIP >> /var/log/pimap2.log
 			pkill python
 			python /home/pi/pi-map/startLED.py
-			mv /var/www/html/commandqueue/$deviceIP.cmd /var/www/html/commandqueue/running/
 			rm /var/www/html/commandqueue/running/default.cmd
 			sudo nmap -O -oX /var/www/html/$deviceIP.xml $deviceIP
 			python /home/pi/pi-map/stopLED.py
+			touch /var/www/html/commandqueue/running/default.cmd
 			rm /var/www/html/commandqueue/running/$deviceIP.cmd
 		done
+		rm /var/www/html/commandqueue/running/default.cmd
 #Check if there is a current job in the running queue
-elif [ -e /var/www/html/commandqueue/running/*.cmd ]
+#elif [ -e /var/www/html/commandqueue/running/*.cmd ]
+elif compgen -G "/var/www/html/commandqueue/running/*.cmd" > /dev/null
 	then
 		#Essentially wait until running jobs finish
 		echo "" > /dev/null
-#If all other commands have run then display the device IP back on the screen
+#If all other commands have run then display the device IP back on the screen, IP returned by getIP.sh script
 else
 	sudo touch /var/www/html/commandqueue/running/default.cmd
 	/usr/bin/python /home/pi/pi-map/ipLED.py $(bash /home/pi/pi-map/getIP.sh)
